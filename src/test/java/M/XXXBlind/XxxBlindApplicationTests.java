1eecebeb9062288c181b3aa0153efff3a65f3a46
kbwoo1065 <kbwoo1065@gmail.com>
2021-11-19 18:46:28 +0900

    init

---
+++ src/test/java/M/XXXBlind/XxxBlindApplicationTests.java
@@ -0,0 +1,13 @@
+package M.XXXBlind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxxBlindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
